1ba25aac8982fd32d46930f4eca5d1638887e2a0
lolonois <thomas.mebarki@protonmail.com>
2023-05-10 12:12:44 +0200

    maj

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, current_app, jsonify
 import pickle
+import os
 import json
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -120,5 +121,6 @@ def predict():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
